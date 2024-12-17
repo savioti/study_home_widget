98ef28c111e749489e6788d3a138076a4a9f5fc2
savioti <matheussavioti@gmail.com>
2024-12-17 15:06:32 -0500

    config

---
+++ ios/NewsWidgets/NewsWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NewsWidgetsBundle.swift
+//  NewsWidgets
+//
+//  Created by user253537 on 12/17/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewsWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        NewsWidgets()
+    }
+}
